db39e6aff486551e4c87d9258446bcd608a3a527
Duc Nam <namnd0210@gmail.com>
2021-10-22 23:22:15 +0700

    build: add husky

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,30 +1,30 @@
-import User from "../models/User";
+import User from '../models/User'
 
 export const register = async (req, res) => {
   try {
     User.findOne({ username: req.body.username }).then((user) => {
       if (user) {
-        return res.status(400).json({ err: "Username already exists !!" });
+        return res.status(400).json({ err: 'Username already exists !!' })
       } else {
         const newUser = new User({
           name: req.body.name,
           username: req.body.username,
           email: req.body.email,
-          password: req.body.password,
-        });
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then((user) => res.json({ success: true, user }))
-              .catch((err) => res.status(400).json({ success: false, err }));
-          });
-        });
+          password: req.body.password
+        })
+        // bcrypt.genSalt(10, (err, salt) => {
+        //   bcrypt.hash(newUser.password, salt, (err, hash) => {
+        //     if (err) throw err;
+        //     newUser.password = hash;
+        //     newUser
+        //       .save()
+        //       .then((user) => res.json({ success: true, user }))
+        //       .catch((err) => res.status(400).json({ success: false, err }));
+        //   });
+        // });
       }
-    });
+    })
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(404).json({ message: err.message })
   }
-};
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,9 @@
-import express from "express";
-import { register } from "../controllers/user";
+import express from 'express'
+import { register } from '../controllers/user'
 
-const router = express.Router();
+const router = express.Router()
 
-router.post("/register", register);
+router.post('/register', register)
 // router.post("/login", login);
 
-export default router;
+export default router
