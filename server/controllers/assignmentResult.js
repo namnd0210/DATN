ca34c8534c065448fd6bbbea47ca8107291a0f23
Duc Nam <namnd0210@gmail.com>
2021-11-26 14:34:36 +0700

    feat: get assignment result by assginment id api

--- server/controllers/assignmentResult.js
+++ server/controllers/assignmentResult.js
@@ -3,11 +3,22 @@ import AssignmentResult from '../models/AssignmentResult';
 export const getResultByUserId = async (req, res) => {
   let count = await AssignmentResult.countDocuments();
   const id = req.params.id;
-  AssignmentResult.find({ _id: id })
+  AssignmentResult.find({ created_by: id })
     .then((data) => {
       res.status(200).json({
         data,
         total: count,
+      });
+    })
+    .catch((err) => res.status(400).json({ err }));
+};
+
+export const getResultByAssignmentId = async (req, res) => {
+  const id = req.params.id;
+  AssignmentResult.findOne({ assignment: id })
+    .then((data) => {
+      res.status(200).json({
+        data,
       });
     })
     .catch((err) => res.status(400).json({ err }));
--- server/routes/assignmentResult.js
+++ server/routes/assignmentResult.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
-import { createResult, getResultByUserId } from '../controllers/assignmentResult';
+import { createResult, getResultByAssignmentId, getResultByUserId } from '../controllers/assignmentResult';
 
 const router = express.Router();
 
 // router.get('/', getAllResults);
-// router.get('/assignment/:id', getAllByAssignmentId);
 // router.get('/class/:id', getAllByClassId);
+router.get('/assignment/:id', getResultByAssignmentId);
 router.get('/user/:id', getResultByUserId);
 router.post('/', createResult);
 
