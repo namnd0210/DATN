ab48b2b2a5c6828871156dcb5db5ad9eb31f2e7c
Duc Nam <namnd0210@gmail.com>
2021-12-20 19:45:05 +0700

    feat: mock update profile

--- src/container/user/Profile/index.tsx
+++ src/container/user/Profile/index.tsx
@@ -1,41 +1,149 @@
 import './style.scss';
 
-import { DeleteOutlined, EditOutlined, PlayCircleOutlined, PlusCircleOutlined } from '@ant-design/icons';
-import { Button, Col, Popconfirm, Row, Table } from 'antd';
+import { UploadOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Button, Col, Form, Input, message, Popconfirm, Row, Upload } from 'antd';
 import { PageHeaderLayout } from 'common/PageHeaderLayout';
+import { getFirebaseImageUrl } from 'constants/firebase.config';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { deleteAssignment, getAllAssignments } from 'redux/assignment/actions';
 import { useSelector } from 'redux/reducer';
 import { AssignmentProps } from 'types/redux';
 
+const dummyRequest = ({ file, onSuccess }: any) => {
+  setTimeout(() => {
+    onSuccess('ok');
+  }, 0);
+};
+
 const Profile = () => {
   const dispatch = useDispatch();
-  const [selectedAssignment, setSelectedAssignment] = useState<AssignmentProps | undefined>(undefined);
-  const { isAdmin, isTeacher } = useSelector((state) => state.auth);
+  const { name, email, id: userId } = useSelector((state) => state.auth.user);
+  const [file, setFile] = useState<any>(
+    getFirebaseImageUrl({
+      id: userId,
+      path: ['avatars'],
+    }) ?? '',
+  );
 
-  useEffect(() => {
-    dispatch(getAllAssignments());
-    // eslint-disable-next-line
-  }, []);
+  //TODO: upload single image
+
+  // const handleSubmit = async () => {
+  //   let fileNames: string[] = [];
+  //   let promises: any[] = [];
+
+  //   await files
+  //     .map((file) => file.originFileObj)
+  //     .map((file) => {
+  //       const fileName = handleFileType(file.type) + '_' + uuidv4();
+  //       fileNames.push(fileName);
+
+  //       const uploadTask = storage.ref(`/assignments/${assignmentId}/${userId}/${fileName}`).put(file);
+  //       promises.push(uploadTask);
+
+  //       return uploadTask.on(
+  //         'state_changed',
+  //         (snapshot) => {},
+  //         (error) => {
+  //           console.log(error);
+  //         },
+  //       );
+  //     });
+
+  //   Promise.all(promises)
+  //     .then(() => {
+  //       const payload = {
+  //         files: fileNames,
+  //         assignment: assignmentId,
+  //         class: classId,
+  //         created_by: userId,
+  //       };
+
+  //       isEqual(currentAssignmentResult, {})
+  //         ? dispatch(createAssignmentResult(payload))
+  //         : dispatch(updateAssignmentResult({ ...payload, _id: currentAssignmentResult._id }));
+
+  //       setFiles([]);
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
+
+  const onFinish = (values: any) => {
+    console.log(values);
+  };
+
+  const uploadProps = {
+    name: 'file',
+    onChange(info: any) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+
+    customRequest: dummyRequest,
+  };
 
   return (
     <Row className="card-list" gutter={[0, 5]}>
       <Col span={24}>
         <PageHeaderLayout title="Thông tin cá nhân" subtitle="" text="" />
+
+        <Form
+          onFinish={onFinish}
+          initialValues={{
+            name: name ?? '',
+            email: email ?? '',
+          }}
+        >
+          <Row gutter={16} style={{ background: '#fff', margin: '1rem 0' }}>
+            <Col xs={24}>
+              <div className="add-exam-item">
+                <div className="add-exam-item__label">Ảnh đại diện</div>
+                <div>
+                  <Avatar
+                    src={file}
+                    style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                    size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+                    icon={<UserOutlined />}
+                  />
+
+                  <Form.Item name="avatar">
+                    <Upload {...uploadProps}>
+                      <Button icon={<UploadOutlined />}>Click to Upload</Button>
+                    </Upload>
+                  </Form.Item>
+                </div>
+              </div>
+
+              <div className="add-exam-item">
+                <div className="add-exam-item__label">Tên bài tập</div>
+                <div className="add-exam-item__main">
+                  <Form.Item name="name">
+                    <Input />
+                  </Form.Item>
+                </div>
+              </div>
+
+              <div className="add-exam-item">
+                <div className="add-exam-item__label">Tên bài tập</div>
+                <div className="add-exam-item__main">
+                  <Form.Item name="email">
+                    <Input />
+                  </Form.Item>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Form>
       </Col>
       <Col span={24}>
-        {isAdmin || isTeacher ? (
-          <Button
-            onClick={() => {
-              setSelectedAssignment(undefined);
-            }}
-            type="dashed"
-            style={{ width: '100%', margin: '10px 0 10px 0' }}
-          >
-            <PlusCircleOutlined /> Tạo bài tập mới
-          </Button>
-        ) : null}
+        <Button type="primary" onClick={() => {}}>
+          Cập nhật
+        </Button>
       </Col>
     </Row>
   );
